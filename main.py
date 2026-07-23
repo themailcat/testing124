27cdb83af5a5405df5e5316aecd1bc00b5a73d02
themailcat <themailcat553@gmail.com>
2026-07-23 17:29:34 +0800

    fixed main.py code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-!/usr/bin/env pybricks micro-python
+!/usr/bin/env python3
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                    InfraredSensor, UltrasonicSensor, GyroSensor)
